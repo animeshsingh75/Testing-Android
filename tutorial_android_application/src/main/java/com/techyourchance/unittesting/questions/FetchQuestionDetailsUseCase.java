3dc18c005039c930e913a324674e981fb0d13f2a
Animesh <animeshsingh75@gmail.com>
2023-11-21 05:49:11 -0700

    Added unit test test to accommodate caching

--- tutorial_android_application/src/main/java/com/techyourchance/unittesting/questions/FetchQuestionDetailsUseCase.java
+++ tutorial_android_application/src/main/java/com/techyourchance/unittesting/questions/FetchQuestionDetailsUseCase.java
@@ -5,27 +5,41 @@ import com.techyourchance.unittesting.common.time.TimeProvider;
 import com.techyourchance.unittesting.networking.questions.FetchQuestionDetailsEndpoint;
 import com.techyourchance.unittesting.networking.questions.QuestionSchema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FetchQuestionDetailsUseCase extends BaseObservable<FetchQuestionDetailsUseCase.Listener> {
 
     public interface Listener {
         void onQuestionDetailsFetched(QuestionDetails questionDetails);
+
         void onQuestionDetailsFetchFailed();
     }
 
+    private static final long CACHE_TIMEOUT_MS = 60000;
+
     private final FetchQuestionDetailsEndpoint mFetchQuestionDetailsEndpoint;
     private final TimeProvider mTimeProvider;
+    private final Map<String, QuestionDetailsCacheEntry> mQuestionDetailsCache = new HashMap<>();
 
-    public FetchQuestionDetailsUseCase(FetchQuestionDetailsEndpoint fetchQuestionDetailsEndpoint,
-                                       TimeProvider timeProvider) {
+    public FetchQuestionDetailsUseCase(FetchQuestionDetailsEndpoint fetchQuestionDetailsEndpoint, TimeProvider timeProvider) {
         mFetchQuestionDetailsEndpoint = fetchQuestionDetailsEndpoint;
         mTimeProvider = timeProvider;
     }
 
-    public void fetchQuestionDetailsAndNotify(String questionId) {
+    public void fetchQuestionDetailsAndNotify(final String questionId) {
+        if(serveQuestionDetailsFromCacheIfValid(questionId)){
+            return;
+        }
         mFetchQuestionDetailsEndpoint.fetchQuestionDetails(questionId, new FetchQuestionDetailsEndpoint.Listener() {
             @Override
             public void onQuestionDetailsFetched(QuestionSchema question) {
-                notifySuccess(question);
+                QuestionDetailsCacheEntry cacheEntry = new QuestionDetailsCacheEntry(
+                        schemaToQuestionDetails(question),
+                        mTimeProvider.getCurrentTimestamp()
+                );
+                mQuestionDetailsCache.put(questionId, cacheEntry);
+                notifySuccess(cacheEntry.mQuestionDetails);
 
             }
 
@@ -35,6 +49,16 @@ public class FetchQuestionDetailsUseCase extends BaseObservable<FetchQuestionDet
             }
         });
     }
+    private boolean serveQuestionDetailsFromCacheIfValid(String questionId) {
+        final QuestionDetailsCacheEntry cachedQuestionDetailsEntry = mQuestionDetailsCache.get(questionId);
+        if (cachedQuestionDetailsEntry != null
+                && mTimeProvider.getCurrentTimestamp() < cachedQuestionDetailsEntry.mCachedTimestamp + CACHE_TIMEOUT_MS) {
+            notifySuccess(cachedQuestionDetailsEntry.mQuestionDetails);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     private void notifyFailure() {
         for (Listener listener : getListeners()) {
@@ -42,14 +66,26 @@ public class FetchQuestionDetailsUseCase extends BaseObservable<FetchQuestionDet
         }
     }
 
-    private void notifySuccess(QuestionSchema questionSchema) {
+    private void notifySuccess(QuestionDetails questionDetails) {
         for (Listener listener : getListeners()) {
-            listener.onQuestionDetailsFetched(
-                    new QuestionDetails(
-                            questionSchema.getId(),
-                            questionSchema.getTitle(),
-                            questionSchema.getBody()
-                    ));
+            listener.onQuestionDetailsFetched(new QuestionDetails(questionDetails.getId(), questionDetails.getTitle(), questionDetails.getBody()));
+        }
+    }
+
+    private QuestionDetails schemaToQuestionDetails(QuestionSchema questionSchema) {
+        return new QuestionDetails(
+                questionSchema.getId(),
+                questionSchema.getTitle(),
+                questionSchema.getBody()
+        );
+    }
+    private static class QuestionDetailsCacheEntry {
+        private final QuestionDetails mQuestionDetails;
+        private final long mCachedTimestamp;
+
+        private QuestionDetailsCacheEntry(QuestionDetails questionDetails, long cachedTimestamp) {
+            mQuestionDetails = questionDetails;
+            mCachedTimestamp = cachedTimestamp;
         }
     }
 }
